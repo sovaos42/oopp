9d149b3ee7ba7f026e9fa6fc89fc1d56fb2654b8
sovaos42 <sovaos42@gmail.com>
2022-09-15 23:04:22 +0700

    part6

--- locallibrary/locallibrary/catalog/urls.py
+++ locallibrary/locallibrary/catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from django.conf.urls import url
+from django.urls import re_path
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
 ]
