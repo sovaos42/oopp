f3d95483c725506d7d6b401a174b417d3909d1ea
sovaos42 <sovaos42@gmail.com>
2022-09-16 22:55:02 +0700

    part7

--- locallibrary/locallibrary/catalog/urls.py
+++ locallibrary/locallibrary/catalog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     re_path(r'^$', views.index, name='index'),
     re_path(r'^books/$', views.BookListView.as_view(), name='books'),
     re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    re_path(r'^author/$', views.AuthorListView.as_view(), name='author'),
+    re_path(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
 ]
--- locallibrary/locallibrary/catalog/views.py
+++ locallibrary/locallibrary/catalog/views.py
@@ -16,13 +16,15 @@ def index(request):
     num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
     num_genre=Genre.objects.count()
     num_fbook=Book.objects.filter(title__iregex='МЫ').count()
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
 
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genre':num_genre, 'num_fbook':num_fbook },
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genre':num_genre, 'num_fbook':num_fbook, 'num_visits':num_visits },
     )
 
 from django.views import generic
@@ -33,3 +35,10 @@ class BookListView(generic.ListView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
